7af9ff38582c50384347e782d82ca7247d9fb37f
viet0103 <nguyenvanhoang11446@gmail.com>
2023-08-05 14:42:20 +0700

    customize scss and reset css

---
+++ Client/src/components/GlobalStyles/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import "~/components/GlobalStyles/GlobalStyles.scss"
+
+GlobalStyles.propTypes = {
+    
+};
+
+function GlobalStyles({children}) {
+    return (
+        children
+    );
+}
+
+export default GlobalStyles;
